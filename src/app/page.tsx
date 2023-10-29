928688afd0bb9b004baf308075629b3f8a567937
Hiroyuki Ota <hota@flywheel.jp>
2023-10-29 21:30:31 +0900

    Fill profile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,29 @@
 function App() {
   return (
-    <div className="max-w-7xl p-10 mx-auto prose">
+    <div className="max-w-4xl p-10 mx-auto prose">
       <header>
         <h1>hota.dev</h1>
       </header>
       <main>
-        <p>hello, world</p>
+        <h2>Profile</h2>
         <ul>
-          <li>test</li>
-          <li>test</li>
-          <li>test</li>
+          <li>Name: Hiroyuki Ota</li>
+          <li>Location: Tokyo, Japan</li>
+          <li>Job: Software Engineer / Engineering Manager</li>
+          <li>
+            Skills: TypeScript, Java, Python, Kotlin, SQL, React, Next.js,
+            Micronaut, Spring Boot, MySQL, Kubernetes, AWS
+          </li>
+          <li>
+            GitHub: <a href="https://github.com/hota911">hota911</a>
+          </li>
+          <li>
+            LinkedIn:{" "}
+            <a href="https://www.linkedin.com/in/hiroyukiota/">hiroyukiota</a>
+          </li>
+          <li>
+            Twitter: <a href="https://twitter.com/hota911">hota911</a>
+          </li>
         </ul>
       </main>
     </div>
