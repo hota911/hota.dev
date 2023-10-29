a0245e9dfd2eb99dceb55c586da2a966be28a639
Hiroyuki Ota <hota@flywheel.jp>
2023-10-29 21:13:53 +0900

    Minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
-"use client"
-
 function App() {
   return (
-    <div className="max-w-7xl p-10 mx-auto">
+    <div className="max-w-7xl p-10 mx-auto prose">
       <header>
         <h1>hota.dev</h1>
       </header>
       <main>
         <p>hello, world</p>
+        <ul>
+          <li>test</li>
+          <li>test</li>
+          <li>test</li>
+        </ul>
       </main>
     </div>
   )
