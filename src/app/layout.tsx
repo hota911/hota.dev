cabdb26ce2d511d41e58c268f402a6ac1cbe07c0
Hiroyuki Ota <hota@flywheel.jp>
2023-10-29 23:40:41 +0900

    Add playground page and header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google"
 import "./globals.css"
+import Link from "next/link"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -15,7 +16,26 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="max-w-2xl p-10 mx-auto">
+          <header>
+            <div className="container mx-auto flex justify-between items-baseline prose prose-a:no-underline">
+              <Link href="/">
+                <h1 className="">hota.dev</h1>
+              </Link>
+              <nav>
+                <Link href="/" className="px-3">
+                  Home
+                </Link>
+                <Link href="/playground" className="px-3">
+                  Playground
+                </Link>
+              </nav>
+            </div>
+          </header>
+          {children}
+        </div>
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,27 @@
 function App() {
   return (
-    <div className="max-w-4xl p-10 mx-auto prose">
-      <header>
-        <h1>hota.dev</h1>
-      </header>
-      <main>
-        <h2>Profile</h2>
-        <ul>
-          <li>Name: Hiroyuki Ota</li>
-          <li>Location: Tokyo, Japan</li>
-          <li>Job: Software Engineer / Engineering Manager</li>
-          <li>
-            Skills: TypeScript, Java, Python, Kotlin, SQL, React, Next.js,
-            Micronaut, Spring Boot, MySQL, Kubernetes, AWS
-          </li>
-          <li>
-            GitHub: <a href="https://github.com/hota911">hota911</a>
-          </li>
-          <li>
-            LinkedIn:{" "}
-            <a href="https://www.linkedin.com/in/hiroyukiota/">hiroyukiota</a>
-          </li>
-          <li>
-            Twitter: <a href="https://twitter.com/hota911">hota911</a>
-          </li>
-        </ul>
-      </main>
-    </div>
+    <main className="prose">
+      <h2>Profile</h2>
+      <ul>
+        <li>Name: Hiroyuki Ota</li>
+        <li>Location: Tokyo, Japan</li>
+        <li>Job: Software Engineer / Engineering Manager</li>
+        <li>
+          Skills: TypeScript, Java, Python, Kotlin, SQL, React, Next.js,
+          Micronaut, Spring Boot, MySQL, Kubernetes, AWS
+        </li>
+        <li>
+          GitHub: <a href="https://github.com/hota911">hota911</a>
+        </li>
+        <li>
+          LinkedIn:{" "}
+          <a href="https://www.linkedin.com/in/hiroyukiota/">hiroyukiota</a>
+        </li>
+        <li>
+          Twitter: <a href="https://twitter.com/hota911">hota911</a>
+        </li>
+      </ul>
+    </main>
   )
 }
 
